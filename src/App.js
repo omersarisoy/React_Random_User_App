c78b5d4f15a1c0c16e77f9f7913fa113ddf28a8c
omersarisoy <omersarisoy1993@gmail.com>
2021-09-12 17:13:12 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState, useEffect } from "react";
+import email from "./assets/email.svg"
+import location from "./assets/location.svg"
+import phoneNumber from "./assets/phone.svg"
 
-function App() {
+const App = () => {
+  const [user, SetUser] = useState([]);
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  const fetchUser = () => {
+    fetch("https://randomuser.me/api/")
+      .then((res) => res.json())
+      .then((data) => {
+        SetUser(data.results);
+        console.log(data.results);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {user?.map((item,index) => (
+        <div key={index} className="card-wrap">
+
+          <div className="pic-name">
+            <img src={item.picture.large} alt="profile" className="profil-pic" />
+            <h2>{item.name.title} {item.name.first} {item.name.last}</h2>
+          </div>
+
+          <div className="par-container">
+            <img src={email} alt="email-pic" className="icon"/>
+            <p className="par">{item.email}</p>
+          </div>
+
+          <div className="par-container">
+            <img src={phoneNumber} alt="Phone-pic" className="icon" />
+            <p className="par" >{item.phone}</p>
+          </div>
+
+          <div className="par-container">
+            <img src={location} alt="location-pic" className="icon"/>
+            <p className="par">{item.location.city} - {item.location.country}</p>
+          </div>
+
+          <p className="age"> Age:{item.dob.age} </p>
+          <p className="register"> Register Date:{item.registered.date.substr(0,10)} </p>
+        </div>
+      ))}
+
+      <button onClick={fetchUser} className="button">
+        {" "}
+        Random User{" "}
+      </button>
     </div>
   );
-}
+};
 
 export default App;
